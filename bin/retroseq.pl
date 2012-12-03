fffa3328784754d76723cb8eaf39f375d575b1c6
Thomas Keane <tk2@sanger.ac.uk>
2012-12-03 14:40:43 +0000

    added GPL licence text

--- bin/retroseq.pl
+++ bin/retroseq.pl
@@ -1,19 +1,19 @@
 #!/usr/bin/env perl
-# 
-# Author:       tk2
-# Maintainer:   tk2
-# Created:      Fri Sep 10 14:07:53 BST 2010 @588 /Internet Time/
-# Updated:      Fri Sep 10 14:08:03 BST 2010 @588 /Internet Time/
-
 =pod
-Status: 
-Should work and compile
-Test: validation and genotyping steps
+This file is part of RetroSeq.
 
-Issues/improvements:
-Genotyping
-    - test it
-    - should dump out genotype likelihoods also
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RetroSeq.  If not, see <http://www.gnu.org/licenses/>.
 =cut
 
 use Carp;
